22038d5c02780dd8e6d145e037d5390d5eef7caf
dibyanshuanand <dibyanshuanand2000@gmail.com>
2019-02-13 20:06:02 +0530

    edit test

--- test.cpp
+++ test.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()
 {
-	char ch = 'f';
-	int chI = ch;
-
-	cout << chI << endl;
+	int a[10] = {1,2,3,4,5,6};
+	cout << sizeof(a)/sizeof(a[0]) << endl;
+	cout << a[4] << endl;
+	for (int i = 0 ; i < 10 ; ++i)
+		cout << a[i] << endl;
 
 	return 0;
 }
